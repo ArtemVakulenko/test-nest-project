b1bd20e55175cd46c68e4115f49b09dd9143de7a
ArtemVakulenko <artemvakulenko42@gmail.com>
2021-07-12 12:37:08 +0300

    init

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CatsController } from './cats/cats.controller';
+import { CatsService } from './cats/cats.service';
+import { UsersController } from './users/users.controller';
+
+@Module({
+  imports: [],
+  controllers: [AppController, CatsController, UsersController],
+  providers: [AppService, CatsService],
+})
+export class AppModule {}
