229d8a1ea15d5f561f0e66a045c80a494c58d8f9
ArtemVakulenko <artemvakulenko42@gmail.com>
2021-07-12 19:53:35 +0300

    boilerplate code for posts and comments

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { CatsModule } from './cats/cats.module';
 import { PostsModule } from './posts/posts.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, CatsModule, PostsModule],
+  imports: [UsersModule, DatabaseModule, CatsModule, PostsModule, CommentsModule],
   controllers: [],
   providers: [],
 })
