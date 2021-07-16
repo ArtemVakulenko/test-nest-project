878900da5ab695ddf13e5b17d0a8945e48d30970
ArtemVakulenko <artemvakulenko42@gmail.com>
2021-07-16 16:21:11 +0300

    added messages module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { PostsModule } from './posts/posts.module';
 import { CommentsModule } from './comments/comments.module';
 import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
+import { MessagesModule } from './messages/messages.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { AppController } from './app.controller';
     PostsModule,
     CommentsModule,
     AuthModule,
+    MessagesModule,
   ],
   controllers: [AppController],
 })
