1566b303864e2cac63dcd5d6ad2efc553105fb5e
ArtemVakulenko <artemvakulenko42@gmail.com>
2021-07-27 17:19:57 +0300

    deleted unnecesary files

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from '../users/users.module';
 import { AuthService } from './auth.service';
-import { LocalStrategy } from './strategies/local.strategy';
 import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
@@ -18,13 +17,7 @@ const jwtConfig = {
 
 @Module({
   imports: [UsersModule, PassportModule, JwtModule.register(jwtConfig)],
-  providers: [
-    AuthService,
-    LocalStrategy,
-    JwtStrategy,
-    GoogleStrategy,
-    FacebookStrategy,
-  ],
+  providers: [AuthService, JwtStrategy, GoogleStrategy, FacebookStrategy],
   controllers: [AuthController],
   exports: [AuthService, JwtModule, GoogleStrategy, FacebookStrategy],
 })
